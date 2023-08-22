1a311951f3c3f8386840551059359f9705d62358
David Waza <wazabanga00@gmail.com>
2023-08-22 15:41:40 +0100

    worked on the landing page

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,150 @@
+"use client";
+import { useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import localFont from "next/font/local";
+import "@/styles/styles.module.css";
+import "swiper/css";
+
+import {
+  Autoplay,
+  Keyboard,
+  Scrollbar,
+  Navigation,
+  Pagination,
+} from "swiper/modules";
+
+const myFonts = localFont({ src: "../../public/fonts/Hannari-Regular.ttf" });
+
+const PortfolioCarousel = () => {
+  useEffect(() => {
+    async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    };
+  });
+
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        centeredSlides={false}
+        slidesPerGroupSkip={1}
+        grabCursor={true}
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          769: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+        scrollbar={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        // autoplay={{
+        //   delay: 3500,
+        //   disableOnInteraction: false,
+        // }}
+        modules={[Autoplay, Keyboard, Scrollbar, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="bg-[black]">
+            <div className="p-10">
+              <p className="text-[#939393] mt-3 font-[700] text-[50px]">
+                Hello,
+              </p>
+              <p className=" text-[#939393] font-[300] text-[30px]">
+                I'm{" "}
+                <span className="text-[burlywood] poppins">
+                  {" "}
+                  Damilare Kolawole
+                </span>
+              </p>
+              <p
+                className="text-[#939393] mt-3 w-[100%] leading-7 text-[24px] poppins"
+                style={myFonts.style}
+              >
+                I am a serial Entrepreneur, Photographer, event planner. i
+                provide high quality services. welcome to my portfolio. This
+                would be the most entertaining site i do have. Thanks to Waza
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="img-container">
+            <div className="img-hover">
+              <figure>
+                <img src="/assets/matts.jpg" />
+              </figure>
+              <div
+                data-scroll
+                data-scroll-speed="0.3"
+                className="img-text poppins text-[burlywood]"
+              >
+                Photograpghy
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="img-container">
+            <div className="img-hover">
+              <figure>
+                <img src="/assets/diamond.jpg" />
+              </figure>
+              <div
+                data-scroll
+                data-scroll-speed="0.3"
+                className="img-text poppins text-white"
+              >
+                Diamonds
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="img-container">
+            <div className="img-hover">
+              <figure>
+                <img src="/assets/gemstone.jpg" />
+              </figure>
+              <div
+                data-scroll
+                data-scroll-speed="0.3"
+                className="img-text poppins text-[#4a2c2a]"
+              >
+                Gem Stones
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="img-container">
+            <div className="img-hover">
+              <figure>
+                <img src="/assets/e-commerce.jpg" />
+              </figure>
+              <div
+                data-scroll
+                data-scroll-speed="0.3"
+                className="img-text poppins text-[burlywood]"
+              >
+               E-commerce
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+export default PortfolioCarousel;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+export default function NavAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "transparent", marginTop: 3 }}
+      >
+        <Toolbar>
+          <Box>
+            <FacebookTwoToneIcon
+              sx={{ fontSize:'18px', color: "burlywood", cursor: "pointer" }}
+            />
+            <InstagramIcon
+              sx={{ fontSize:'18px', marginLeft: 3, color: "burlywood", cursor: "pointer" }}
+            />
+            <TwitterIcon
+              sx={{ fontSize:'18px', marginLeft: 3, color: "burlywood", cursor: "pointer" }}
+            />
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}
+          >
+            DAMILARE
+          </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2, color: "burlywood" }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
