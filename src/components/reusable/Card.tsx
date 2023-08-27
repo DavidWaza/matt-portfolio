c1eac44b8435c617387cec6662bed745b9534c7a
David Waza <wazabanga00@gmail.com>
2023-08-27 18:52:53 +0100

    added links to profile cards

--- src/components/reusable/Card.tsx
+++ src/components/reusable/Card.tsx
@@ -2,6 +2,7 @@ interface Profile {
   id: number;
   img: string;
   title: string;
+  link:string;
   class: string;
 }
 
@@ -10,30 +11,35 @@ export const profession: Profile[] = [
     id: 1,
     img: "/assets/matts.jpg",
     title: "photography",
+    link: '/photography',
     class: "class-burylwood",
   },
   {
     id: 1,
     img: "/assets/diamond-ring.jpg",
     title: "diamonds",
+    link:'diamonds',
     class: "class-coal",
   },
   {
     id: 1,
     img: "/assets/gemstone.jpg",
     title: "precious stones",
+    link:'/precious-stones',
     class: "class-coffee",
   },
   {
     id: 1,
     img: "/assets/e-commerce.jpg",
     title: "e-commerce",
+    link:'/e-commerce',
     class: "class-white",
   },
   {
     id: 1,
     img: "/assets/frontend.jpg",
     title: "website development",
+    link:'web-dev',
     class: "class-white",
   },
 ];
--- src/components/reusable/CardListItem.tsx
+++ src/components/reusable/CardListItem.tsx
@@ -3,6 +3,8 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import Tilt from "react-parallax-tilt";
 import localFont from "next/font/local";
+import Image from "next/image";
+import Link from "next/link";
 
 // interface CardListItemProps {
 //   profession: Profile[];
@@ -10,28 +12,37 @@ import localFont from "next/font/local";
 
 const myFonts = localFont({ src: "../../../public/fonts/Hannari-Regular.ttf" });
 
-const CardListItem = ({ profession }: {profession:any}) => {
+const CardListItem = ({ profession }: { profession: any }) => {
   const [scale, setScale] = useState(1.15);
   return (
     <>
-      {profession.map((profile:any) => (
+      {profession.map((profile: any) => (
         <div className="img-container " key={profile.id}>
           <div className="img-hover">
             <Tilt
               className="parallax-effect"
               scale={scale}
               transitionSpeed={2500}
-            //   perspective={500}
+              //   perspective={500}
             >
-              <figure>
-                <img src={profile.img} />
-                {/* <p
+              <Link href={profile.link}>
+                <figure>
+                  <Image
+                    alt={profile.title}
+                    src={profile.img}
+                    width={410}
+                    height={410}
+                    loading="lazy"
+                  />
+                  {/* <p
                   className="img-text-parallax"
                   style={myFonts.style}
                 >
                   {profile.title}
                 </p> */}
-              </figure>
+                </figure>
+              </Link>
+
               <div
                 data-scroll
                 data-scroll-speed="0.3"
